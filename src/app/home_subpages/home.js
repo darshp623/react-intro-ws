4f3f4e38afbc882c94f23ae9b046fa7385cc489f
darshp623 <darshp717@gmail.com>
2024-09-28 15:26:26 -0400

    fixed css
    title

--- src/app/home_subpages/home.js
+++ src/app/home_subpages/home.js
@@ -5,11 +5,46 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      easing: "ease-in-out",
+      once: true,
+    });
+  }, []);
 
   return (
     <div className="home-page" style={{ background: "radial-gradient(125% 125% at 50% 10%, #000 40%, rgb(0, 155, 227) 100%)",}}>
       <div className="home-container">
+        {/* Profile Picture Section */}
+        <div className="section__pic-container" data-aos="fade-right">
+          <img src="./profile-pic.PNG" alt="Darsh Patel's Profile Picture" />
+        </div>
+        
+        {/* Text Section */}
+        <div className="section__text" data-aos="fade-left">
+          <p className="section__text__p1 light-mode-text">Hello, I'm</p>
+          <h1 className="title">Darsh Patel</h1>
+          <p className="section__text__p2 light-mode-text">Computer Science Undergrad</p>
+
+          {/* Button Container */}
+          <div className="btn-container">
+            <button className="btn btn-color-1" onClick={() => window.open('./assets/resume.pdf')}>
+              Download CV
+            </button>
+            <button className="btn btn-color-2" onClick={() => location.href = './#contact'}>
+              Contact Info
+            </button>
+          </div>
+
+          {/* Social Links */}
+          <div id="socials-container">
+            <img src="./linkedin.png" alt="LinkedIn" className="icon" onClick={() => window.open('https://www.linkedin.com/in/darshp623/')}/>
+            <img src="./github.PNG" alt="GitHub" className="icon" onClick={() => window.open('https://github.com/darshp623/')}/>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
