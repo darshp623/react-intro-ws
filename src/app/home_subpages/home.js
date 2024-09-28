76bb631c59a5ea0a8be387b97fb3f6c55eaf140b
darshp623 <darshp717@gmail.com>
2024-09-27 21:07:45 -0400

    base workshop
    title

--- src/app/home_subpages/home.js
+++ src/app/home_subpages/home.js
@@ -1,48 +1,14 @@
 "use client";
-import Image from "next/image";
 import "../css/home.css";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default function Home() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1200,
-      easing: "ease-in-out",
-      once: true,
-    });
-  }, []);
-
-  const [isMobile, setIsMobile] = useState(true);
-
-  // handle resize
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    // Initial check on component mount
-    handleResize();
-
-    // Add event listener for resize events
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
-    <div className="home-page">
-      <div
-        className="home-container"
-        style={{
-          background:
-            "radial-gradient(125% 125% at 50% 10%, #000 40%, rgb(0, 155, 227) 100%)",
-        }}
-      >
+    <div className="home-page" style={{ background: "radial-gradient(125% 125% at 50% 10%, #000 40%, rgb(0, 155, 227) 100%)",}}>
+      <div className="home-container">
       </div>
     </div>
   );
